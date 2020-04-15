9c57e84e4e140776609ff66fef9b219698badf51
Misael Ferreira <misael.ferreira@gmail.com>
2020-04-15 00:34:00 -0300

    initial commit

---
+++ src/test/java/com/fersoft/mercado/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fersoft.mercado.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
